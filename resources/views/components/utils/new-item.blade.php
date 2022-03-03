649184322ea3fb3f6447a9a9dfd507b6b7dfc429
Frank <fcpaucar@gmail.com>
2022-03-03 12:07:45 -0500

    style(utils): se cambió el mensaje badge por un icono

--- resources/views/components/utils/new-item.blade.php
+++ resources/views/components/utils/new-item.blade.php
@@ -1,5 +1,7 @@
 @if(\Carbon\Carbon::parse($date)->diffInSeconds(\Carbon\Carbon::now()) <= 90)
-    <x-utils.badge class="bg-green-100 text-green-700 font-semibold text-xs">
-        Nuevo
-    </x-utils.badge>
+    <div class="-mt-3">
+        <svg class="text-green-500 animate-pulse" fill="currentColor" viewBox="0 0 16 16" width="16" height="16">
+            <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8z"></path>
+        </svg>
+    </div>
 @endif
