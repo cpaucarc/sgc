bd8339b890aca92f17c5b3b6eaec7471771f1607
Frank <fcpaucar@gmail.com>
2022-03-03 15:33:57 -0500

    style: se modificó el color del icono izquierdo de los nav-links en admin

--- resources/views/components/utils/links/nav-link.blade.php
+++ resources/views/components/utils/links/nav-link.blade.php
@@ -2,13 +2,13 @@
 
 @php
     $classes = ($active ?? false)
-                ? 'relative py-2 px-2 text-sm rounded-md flex flex-1 w-full items-center justify-between bg-stone-100 text-stone-800 font-bold cursor-pointer tracking-wide'
-                : 'py-2 px-2 text-sm rounded-md flex flex-1 w-full items-center justify-between bg-white hover:bg-stone-100 text-gray-600 font-semibold hover:text-gray-700 cursor-pointer whitespace-nowrap';
+                ? 'relative py-2 px-2 text-sm rounded-md flex flex-1 w-full items-center justify-between bg-zinc-100 text-stone-800 font-bold cursor-pointer tracking-wide'
+                : 'py-2 px-2 text-sm rounded-md flex flex-1 w-full items-center justify-between bg-white hover:bg-zinc-100 text-gray-600 font-semibold hover:text-gray-700 cursor-pointer whitespace-nowrap';
 @endphp
 
 <a {{ $attributes->merge(['class' => $classes]) }}>
     @if($active)
-        <div class="absolute -left-2 h-6 w-1 bg-sky-600 rounded-lg"></div>
+        <div class="absolute -left-2 h-6 w-1 bg-blue-600 rounded-lg"></div>
     @endif
     {{ $slot }}
 </a>
