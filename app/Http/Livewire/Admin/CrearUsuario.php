1415f94c80b286f6e358a0a100e66af959183bef
Frank Paucar <fcpaucar@gmail.com>
2022-04-13 10:58:43 -0500

    feat: se implemnetó la busqueda por DNI en la sección de registro de usuarios

--- app/Http/Livewire/Admin/CrearUsuario.php
+++ app/Http/Livewire/Admin/CrearUsuario.php
@@ -11,7 +11,8 @@ use Livewire\Component;
 class CrearUsuario extends Component
 {
     public $open = false;
-    public $nombres, $dni, $correo, $contrasena = "password";
+    public $nombres, $dni, $correo, $contrasena;
+    public $mensaje = null;
 
     protected $rules = [
         'nombres' => 'required|max:250',
@@ -46,6 +47,27 @@ class CrearUsuario extends Component
 
         $this->reset('open', 'nombres', 'dni', 'correo', 'contrasena');
         $this->emitTo('admin.lista-usuarios', 'render');
+    }
+
+    public function buscar()
+    {
+        if (strlen($this->dni) === 8) {
+            $response = Http::withToken(env('OGE_TOKEN'))
+                ->get(env('OGE_API') . 'ensenianza_aprendizaje/01?codigo=' . $this->dni);
+            $response = $response->json();
+
+            if (isset($response['dni'])) {
+                $this->mensaje = null;
+                $this->nombres = ucwords(strtolower($response['nombre_completo']));
+                $this->correo = strtolower($response['correo_institucional']);
+                $this->contrasena = $this->dni;
+            } else {
+                $this->mensaje = "No hay registros que coincidan con el DNI " . $this->dni;
+                $this->reset('nombres', 'dni', 'correo', 'contrasena');
+            }
+        } else {
+            $this->mensaje = "El DNI deberia tener 8 digitos, solo hay " . strlen($this->dni);
+        }
     }
 
 }
--- resources/views/livewire/admin/crear-usuario.blade.php
+++ resources/views/livewire/admin/crear-usuario.blade.php
@@ -16,13 +16,18 @@
                 <div class="w-full">
                     <x-jet-label for="dni" value="DNI del usuario"/>
                     <div class="flex gap-x-2">
-                        <x-jet-input id="dni" autocomplete="off" class="flex-1" type="text"
+                        <x-jet-input id="dni" autocomplete="off" maxlength="8" class="flex-1" type="text"
                                      wire:model.defer="dni"/>
-                        <x-utils.buttons.default class="">
+                        <x-utils.buttons.default wire:click="buscar">
                             <x-icons.search class="w-4 h-4"/>
                         </x-utils.buttons.default>
                     </div>
                     <x-jet-input-error for="dni"/>
+                    @if($mensaje)
+                        <p class="w-full text-sm text-rose-600">
+                            {{ $mensaje }}
+                        </p>
+                    @endif
                 </div>
                 <div class="w-full">
                     <x-jet-label for="nombres" value="Apellidos y Nombres del Usuario"/>
