6254d948ef699036984d30c5fa1ce1548be9d852
Angel Pagola <57104299+angelpagola@users.noreply.github.com>
2022-06-10 17:19:44 -0500

    fix (tpu): mejora la vista principal de index

--- app/Http/Controllers/TituloProfesionalController.php
+++ app/Http/Controllers/TituloProfesionalController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Entidad;
+use App\Models\Escuela;
+use App\Models\Facultad;
+use App\Models\GradoEstudiante;
 use App\Models\Solicitud;
 use App\Models\Tesis;
 use App\Models\User;
@@ -11,6 +14,11 @@ use Illuminate\Support\Facades\Auth;
 
 class TituloProfesionalController extends Controller
 {
+    /* Variables
+     * Sirve para saber el usuario a que facultad o escuela pertenece.
+     */
+    public $facultad = null, $escuela = null;
+
     public function index()
     {
         $alumnos = Entidad::query()->where('oficina_id', 9)->pluck('id')->toArray();
@@ -21,12 +29,66 @@ class TituloProfesionalController extends Controller
             return redirect()->route('tpu.request');
         }
 
-        $escuela_id = User::escuelas_id(Auth::user()->id);
+        //Toma los ids de la facultad que pertence el usuario. Retorna en un array.
+        $facultades_id = User::facultades_id(Auth::user()->id);
+        $escuelas_id = User::escuelas_id(Auth::user()->id);
 
-        if (empty($escuela_id))
+        $callback_socicitud = Solicitud::query()
+            ->where('tipo_solicitud_id', 3)// 3 : Título
+            ->withCount('documentos')
+            ->having('documentos_count', '>', 0);
+
+        $callback_titulados = GradoEstudiante::query()->distinct('dni_estudiante')
+            ->where('grado_academico_id', 4); // 4: Titulado
+
+        if (count($escuelas_id) > 0) {
+            $this->escuela = Escuela::query()->whereIn('id', $escuelas_id)->first();
+            $this->facultad = Facultad::query()->whereIn('id', function ($query) {
+                $query->select('facultad_id')->from('escuelas')
+                    ->where('id', $this->escuela->id);
+            })->first();
+
+            $solicitudes = $callback_socicitud->where('escuela_id', $this->escuela->id)->get();
+            $titulados = $callback_titulados->where('escuela_id', $this->escuela->id)->count();
+
+        } elseif (count($facultades_id) > 0) {
+            $this->facultad = Facultad::query()->whereIn('id', $facultades_id)->first();
+
+            $solicitudes = $callback_socicitud->whereIn('escuela_id', function ($query) {
+                $query->select('id')->from('escuelas')
+                    ->where('facultad_id', $this->facultad->id);
+            })->get();
+
+            $titulados = $callback_titulados->whereIn('escuela_id', function ($query) {
+                $query->select('id')->from('escuelas')
+                    ->where('facultad_id', $this->facultad->id);
+            })->count();
+
+        } else {
+//            abort(403, 'No tienes los permisos para estar en esta página');
             return redirect()->route('dashboard');
+        }
 
-        return view('tpu.index', compact('escuela_id'));
+        $solicitudesCompletas = $solicitudes->filter(function ($item) {
+            return $item->documentos_count == 8; // 8 : Requisitos de titulo profesional
+        });
+
+        $solicitudesIncompletas = $solicitudes->filter(function ($item) {
+            return $item->documentos_count < 8; // 8 : Requisitos de titulo profesional
+        });
+
+        $facultad = $this->facultad;
+        $escuela = $this->escuela;
+
+        $incompletas = $solicitudesIncompletas->count();
+        $completas = $solicitudesCompletas->count();
+
+        /* Evia: facultad y escuela
+         * Si el usuario es de tipo facultad, el parametro facultad evia dato y la escuela es nulo.
+         * Si el usuario es de tipo escuela, el parametro facultad evia dato y la escuela tambien envia dato.
+         */
+
+        return view('tpu.index', compact('facultad', 'escuela', 'incompletas', 'completas', 'titulados'));
     }
 
     public function request()
