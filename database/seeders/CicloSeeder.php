7dca36181d3f880bef47e132f7d99748eae1478e
apagolap <apagolap@unasam.edu.pe>
2022-06-10 09:06:10 -0500

    feat: agrega contenido del seeder ciclo

--- database/seeders/CicloSeeder.php
+++ database/seeders/CicloSeeder.php
@@ -14,6 +14,39 @@ class CicloSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $ciclo = [
+            [
+                'romano' => 'I',
+            ],
+            [
+                'romano' => 'II',
+            ],
+            [
+                'romano' => 'III',
+            ],
+            [
+                'romano' => 'IV',
+            ],
+            [
+                'romano' => 'V',
+            ],
+            [
+                'romano' => 'VI',
+            ],
+            [
+                'romano' => 'VII',
+            ],
+            [
+                'romano' => 'VIII',
+            ],
+            [
+                'romano' => 'IX',
+            ],
+            [
+                'romano' => 'X',
+            ],
+        ];
+
+        \App\Models\Ciclo::insert($ciclo);
     }
 }
