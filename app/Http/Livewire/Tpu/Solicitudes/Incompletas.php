80cbda09c3ebe00a5c06cde9b3e89cd1a90db444
Angel Pagola <57104299+angelpagola@users.noreply.github.com>
2022-06-10 18:32:38 -0500

    feat(tpu): agrega el contenido de las vistas de solicitudes completas e incompletas

---
+++ app/Http/Livewire/Tpu/Solicitudes/Incompletas.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Tpu\Solicitudes;
+
+use Livewire\Component;
+
+class Incompletas extends Component
+{
+    public function render()
+    {
+        return view('livewire.tpu.solicitudes.incompletas');
+    }
+}
--- resources/views/tpu/solicitudes/incompletas.blade.php
+++ resources/views/tpu/solicitudes/incompletas.blade.php
@@ -1,3 +1,21 @@
 <x-app-layout>
-    {{$solicitudesIncompletas}}
+    <div class="grid grid-cols-6 gap-12 pt-8">
+        <div class="col-span-2 space-y-4">
+            <div class="text-right">
+                <p class="text-gray-500">Solicitudes con </p>
+                <h1 class="font-bold text-2xl text-gray-700">
+                    requisitos incompletos
+                </h1>
+                <div class="flex justify-end mt-4">
+                    <p class="bg-yellow-200 text-yellow-800 px-2 py-1 rounded-lg w-32">
+                        {{$solicitudesIncompletas->count()}} solicitudes
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-span-4 space-y-6">
+            <livewire:tpu.solicitudes.incompletas :solicitudesIncompletas="$solicitudesIncompletas"/>
+        </div>
+    </div>
 </x-app-layout>
