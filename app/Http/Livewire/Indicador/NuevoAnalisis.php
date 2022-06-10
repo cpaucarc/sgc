0c10a795249adadb18f39f97f768a95f5d0e6dc0
Frank Paucar <fcpaucar@gmail.com>
2022-06-10 09:26:08 -0500

    style: mejora la legibilidad de la funcion obtenerResultados

--- app/Http/Livewire/Indicador/NuevoAnalisis.php
+++ app/Http/Livewire/Indicador/NuevoAnalisis.php
@@ -191,131 +191,133 @@ class NuevoAnalisis extends Component
 
     public function obtenerResultados()
     {
+        $codigo_inicial = $this->indicadorable->indicador->cod_ind_inicial;
+
         // Bienestar: 019 - 020
-        if ($this->indicadorable->indicador->cod_ind_inicial === "IND-017") {
+        if ($codigo_inicial === "IND-017") {
             $res = Medicion::ind17($this->entidad->id, $this->inicio, $this->fin);
-        } elseif ($this->indicadorable->indicador->cod_ind_inicial === "IND-019") {
+        } elseif ($codigo_inicial === "IND-019") {
             $res = Medicion::ind19($this->entidad->id, $this->inicio, $this->fin);
         } // RSU: 048 - 053
-        elseif ($this->indicadorable->indicador->cod_ind_inicial === "IND-048") {
+        elseif ($codigo_inicial === "IND-048") {
             $res = Medicion::ind48($this->tipo == 1, $this->entidad->id, $this->inicio, $this->fin);
-        } elseif ($this->indicadorable->indicador->cod_ind_inicial === "IND-049") {
+        } elseif ($codigo_inicial === "IND-049") {
             $res = Medicion::ind49($this->tipo == 1, $this->entidad->id, $this->inicio, $this->fin);
-        } elseif ($this->indicadorable->indicador->cod_ind_inicial === "IND-050") {
+        } elseif ($codigo_inicial === "IND-050") {
             $res = Medicion::ind50($this->tipo == 1, $this->entidad->id, $this->inicio, $this->fin);
-        } elseif ($this->indicadorable->indicador->cod_ind_inicial === "IND-051") {
+        } elseif ($codigo_inicial === "IND-051") {
             $res = Medicion::ind51($this->tipo == 1, $this->entidad->id, $this->inicio, $this->fin);
-        } elseif ($this->indicadorable->indicador->cod_ind_inicial === "IND-052") {
+        } elseif ($codigo_inicial === "IND-052") {
             $res = Medicion::ind52($this->tipo == 1, $this->entidad->id, $this->inicio, $this->fin);
         } // Investigacion: 044 - 047
-        elseif ($this->indicadorable->indicador->cod_ind_inicial === "IND-044") {
+        elseif ($codigo_inicial === "IND-044") {
             $res = Medicion::ind44($this->tipo == 1, $this->entidad->id, $this->inicio, $this->fin);
-        } elseif ($this->indicadorable->indicador->cod_ind_inicial === "IND-045") {
+        } elseif ($codigo_inicial === "IND-045") {
             $res = Medicion::ind45($this->tipo == 1, $this->entidad->id, $this->inicio, $this->fin);
-        } elseif ($this->indicadorable->indicador->cod_ind_inicial === "IND-046") {
+        } elseif ($codigo_inicial === "IND-046") {
             $res = Medicion::ind46($this->tipo == 1, $this->entidad->id, $this->inicio, $this->fin);
-        } elseif ($this->indicadorable->indicador->cod_ind_inicial === "IND-047") {
+        } elseif ($codigo_inicial === "IND-047") {
             $res = Medicion::ind47($this->tipo == 1, $this->entidad->id, $this->inicio, $this->fin);
         } // Biblioteca: 09 - 015
-        elseif ($this->indicadorable->indicador->cod_ind_inicial === "IND-009") {
+        elseif ($codigo_inicial === "IND-009") {
             $res = Medicion::ind09($this->entidad->id, $this->inicio, $this->fin);
-        } elseif ($this->indicadorable->indicador->cod_ind_inicial === "IND-010") {
+        } elseif ($codigo_inicial === "IND-010") {
             $res = Medicion::ind10($this->entidad->id, $this->inicio, $this->fin);
-        } elseif ($this->indicadorable->indicador->cod_ind_inicial === "IND-011") {
+        } elseif ($codigo_inicial === "IND-011") {
             $res = Medicion::ind11($this->entidad->id, $this->inicio, $this->fin);
-        } elseif ($this->indicadorable->indicador->cod_ind_inicial === "IND-012") {
+        } elseif ($codigo_inicial === "IND-012") {
             $res = Medicion::ind12($this->entidad->id, $this->inicio, $this->fin);
-        } elseif ($this->indicadorable->indicador->cod_ind_inicial === "IND-013") {
+        } elseif ($codigo_inicial === "IND-013") {
             $res = Medicion::ind13($this->entidad->id, $this->inicio, $this->fin);
-        } elseif ($this->indicadorable->indicador->cod_ind_inicial === "IND-014") {
+        } elseif ($codigo_inicial === "IND-014") {
             $res = Medicion::ind14($this->entidad->id, $this->inicio, $this->fin);
         } // Bachiller: 58
-        elseif ($this->indicadorable->indicador->cod_ind_inicial === "IND-058") {
+        elseif ($codigo_inicial === "IND-058") {
             $res = Medicion::ind58($this->tipo == 1, $this->entidad->id, $this->inicio, $this->fin);
         } // Titulo Profesional: 59
-        elseif ($this->indicadorable->indicador->cod_ind_inicial === "IND-059") {
+        elseif ($codigo_inicial === "IND-059") {
             $res = Medicion::ind59($this->tipo == 1, $this->entidad->id, $this->inicio, $this->fin);
-        } elseif ($this->indicadorable->indicador->cod_ind_inicial === "IND-060") {
+        } elseif ($codigo_inicial === "IND-060") {
             $res = Medicion::ind60($this->tipo == 1, $this->entidad->id, $this->inicio, $this->fin);
-        } elseif ($this->indicadorable->indicador->cod_ind_inicial === "IND-061") {
+        } elseif ($codigo_inicial === "IND-061") {
             $res = Medicion::ind61($this->tipo == 1, $this->entidad->id, $this->inicio, $this->fin);
         } // Gestion de Calidad
-        elseif ($this->indicadorable->indicador->cod_ind_inicial === "IND-001") {
+        elseif ($codigo_inicial === "IND-001") {
             $res = Medicion::ind01($this->entidad->id, $this->inicio, $this->fin);
-        } elseif ($this->indicadorable->indicador->cod_ind_inicial === "IND-068") {
+        } elseif ($codigo_inicial === "IND-068") {
             $res = Medicion::ind68($this->entidad->id, $this->inicio, $this->fin);
-        } elseif ($this->indicadorable->indicador->cod_ind_inicial === "IND-070") {
+        } elseif ($codigo_inicial === "IND-070") {
             $res = Medicion::ind70($this->entidad->id, $this->inicio, $this->fin);
         } // Convalidaciones
-        elseif ($this->indicadorable->indicador->cod_ind_inicial === "IND-024") {
+        elseif ($codigo_inicial === "IND-024") {
             $res = Medicion::ind24($this->entidad->id, $this->inicio, $this->fin);
-        } elseif ($this->indicadorable->indicador->cod_ind_inicial === "IND-025") {
+        } elseif ($codigo_inicial === "IND-025") {
             $res = Medicion::ind25($this->entidad->id, $this->inicio, $this->fin);
-        } elseif ($this->indicadorable->indicador->cod_ind_inicial === "IND-026") {
+        } elseif ($codigo_inicial === "IND-026") {
             $res = Medicion::ind26($this->entidad->id, $this->inicio, $this->fin);
         } // Enseñanza y Aprendizaje
-        elseif ($this->indicadorable->indicador->cod_ind_inicial === "IND-032") {
+        elseif ($codigo_inicial === "IND-032") {
             $res = Medicion::ind32($this->entidad->id, $this->semestre_nombre);
-        } elseif ($this->indicadorable->indicador->cod_ind_inicial === "IND-035") {
+        } elseif ($codigo_inicial === "IND-035") {
             $res = Medicion::ind35($this->entidad->id, $this->semestre_nombre);
-        } elseif ($this->indicadorable->indicador->cod_ind_inicial === "IND-036") {
+        } elseif ($codigo_inicial === "IND-036") {
             $res = Medicion::ind36($this->entidad->id, $this->semestre_nombre);
-        } elseif ($this->indicadorable->indicador->cod_ind_inicial === "IND-037") {
+        } elseif ($codigo_inicial === "IND-037") {
             $res = Medicion::ind37($this->entidad->id, $this->semestre_nombre, $this->inicio, $this->fin);
-        } elseif ($this->indicadorable->indicador->cod_ind_inicial === "IND-038") {
+        } elseif ($codigo_inicial === "IND-038") {
             $res = Medicion::ind38($this->entidad->id, $this->semestre_nombre);
         } // Tutoria y Consejeria
-        elseif ($this->indicadorable->indicador->cod_ind_inicial === "IND-054") {
+        elseif ($codigo_inicial === "IND-054") {
             $res = Medicion::ind54($this->entidad->id, $this->semestre_nombre);
-        } elseif ($this->indicadorable->indicador->cod_ind_inicial === "IND-055") {
+        } elseif ($codigo_inicial === "IND-055") {
             $res = Medicion::ind55($this->entidad->id, $this->semestre_nombre);
-        } elseif ($this->indicadorable->indicador->cod_ind_inicial === "IND-056") {
+        } elseif ($codigo_inicial === "IND-056") {
             $res = Medicion::ind56($this->entidad->id, $this->semestre_nombre);
-        } elseif ($this->indicadorable->indicador->cod_ind_inicial === "IND-057") {
+        } elseif ($codigo_inicial === "IND-057") {
             $res = Medicion::ind57($this->entidad->id, $this->semestre_nombre);
         } // Matricula
-        elseif ($this->indicadorable->indicador->cod_ind_inicial === "IND-039") {
+        elseif ($codigo_inicial === "IND-039") {
             $res = Medicion::ind39($this->entidad->id, $this->semestre_nombre);
-        } elseif ($this->indicadorable->indicador->cod_ind_inicial === "IND-040") {
+        } elseif ($codigo_inicial === "IND-040") {
             $res = Medicion::ind40($this->entidad->id, $this->semestre_nombre);
-        } elseif ($this->indicadorable->indicador->cod_ind_inicial === "IND-041") {
+        } elseif ($codigo_inicial === "IND-041") {
             $res = Medicion::ind41($this->entidad->id, $this->semestre_nombre);
-        } elseif ($this->indicadorable->indicador->cod_ind_inicial === "IND-042") {
+        } elseif ($codigo_inicial === "IND-042") {
             $res = Medicion::ind42($this->entidad->id, $this->semestre_nombre);
-        } elseif ($this->indicadorable->indicador->cod_ind_inicial === "IND-043") {
+        } elseif ($codigo_inicial === "IND-043") {
             $res = Medicion::ind43($this->entidad->id, $this->semestre_nombre);
         } // Docente
-        elseif ($this->indicadorable->indicador->cod_ind_inicial === "IND-062") {
+        elseif ($codigo_inicial === "IND-062") {
             $res = Medicion::ind62($this->tipo == 1, $this->entidad->id, $this->semestre_nombre);
-        } elseif ($this->indicadorable->indicador->cod_ind_inicial === "IND-063") {
+        } elseif ($codigo_inicial === "IND-063") {
             $res = Medicion::ind63($this->tipo == 1, $this->entidad->id, $this->semestre_nombre);
-        } elseif ($this->indicadorable->indicador->cod_ind_inicial === "IND-065") {
+        } elseif ($codigo_inicial === "IND-065") {
             $res = Medicion::ind65($this->tipo == 1, $this->entidad->id, $this->semestre_nombre);
-        } elseif ($this->indicadorable->indicador->cod_ind_inicial === "IND-066") {
+        } elseif ($codigo_inicial === "IND-066") {
             $res = Medicion::ind66($this->tipo == 1, $this->entidad->id, $this->semestre_nombre);
-        } elseif ($this->indicadorable->indicador->cod_ind_inicial === "IND-067") {
+        } elseif ($codigo_inicial === "IND-067") {
             $res = Medicion::ind67($this->tipo == 1, $this->entidad->id, $this->semestre_nombre);
-        } elseif ($this->indicadorable->indicador->cod_ind_inicial === "IND-074") {
+        } elseif ($codigo_inicial === "IND-074") {
             $res = Medicion::ind74($this->tipo == 1, $this->entidad->id, $this->semestre_nombre);
-        } elseif ($this->indicadorable->indicador->cod_ind_inicial === "IND-075") {
+        } elseif ($codigo_inicial === "IND-075") {
             $res = Medicion::ind75($this->tipo == 1, $this->entidad->id, $this->semestre_nombre);
-        } elseif ($this->indicadorable->indicador->cod_ind_inicial === "IND-076") {
+        } elseif ($codigo_inicial === "IND-076") {
             $res = Medicion::ind76($this->tipo == 1, $this->entidad->id, $this->semestre_nombre);
-        } elseif ($this->indicadorable->indicador->cod_ind_inicial === "IND-077") {
+        } elseif ($codigo_inicial === "IND-077") {
             $res = Medicion::ind77($this->tipo == 1, $this->entidad->id, $this->semestre_nombre);
-        } elseif ($this->indicadorable->indicador->cod_ind_inicial === "IND-078") {
+        } elseif ($codigo_inicial === "IND-078") {
             $res = Medicion::ind78($this->tipo == 1, $this->entidad->id, $this->semestre_nombre);
         } // Bolsa
-        elseif ($this->indicadorable->indicador->cod_ind_inicial === "IND-021") {
+        elseif ($codigo_inicial === "IND-021") {
             $res = Medicion::ind21($this->entidad->id, $this->inicio, $this->fin);
         } // Convenio
-        elseif ($this->indicadorable->indicador->cod_ind_inicial === "IND-027") {
+        elseif ($codigo_inicial === "IND-027") {
             $res = Medicion::ind27($this->entidad->id, $this->semestre_id);
-        } elseif ($this->indicadorable->indicador->cod_ind_inicial === "IND-028") {
+        } elseif ($codigo_inicial === "IND-028") {
             $res = Medicion::ind28($this->entidad->id, $this->semestre_id);
-        } elseif ($this->indicadorable->indicador->cod_ind_inicial === "IND-029") {
+        } elseif ($codigo_inicial === "IND-029") {
             $res = Medicion::ind29($this->entidad->id, $this->semestre_id);
-        } elseif ($this->indicadorable->indicador->cod_ind_inicial === "IND-030") {
+        } elseif ($codigo_inicial === "IND-030") {
             $res = Medicion::ind30($this->entidad->id, $this->semestre_id);
         }
 
