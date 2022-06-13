cc09f396c9021e38a3dcb8c2a7eb16510d5a7abc
Frank Paucar <fcpaucar@gmail.com>
2022-06-13 17:01:53 -0500

    refactor: cambia de color al texto de blue a gray en su estado base

--- resources/views/components/utils/buttons/invisible.blade.php
+++ resources/views/components/utils/buttons/invisible.blade.php
@@ -1,5 +1,5 @@
 <button {{ $attributes->merge([
-    'class' => 'px-2 py-1 inline-flex items-center text-blue-500 bg-transparent hover:text-blue-600 active:text-blue-700 font-bold whitespace-nowrap transition ease-in-out duration-300'
+    'class' => 'px-2 py-1 inline-flex items-center text-gray-500 bg-transparent hover:text-blue-600 active:text-blue-700 font-bold whitespace-nowrap soft-transition'
     ])
 }}>
     {{$slot}}
