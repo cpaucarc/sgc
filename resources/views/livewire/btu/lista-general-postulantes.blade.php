ee9e8feaa41ae495fe0089bf1f73ac79537838d5
Angel Pagola <57104299+angelpagola@users.noreply.github.com>
2022-06-14 15:51:51 -0500

    feat: muestra mensaje de confirmacion al eliminar registros de la tabla en Bolsa de trabajo.

--- resources/views/livewire/btu/lista-general-postulantes.blade.php
+++ resources/views/livewire/btu/lista-general-postulantes.blade.php
@@ -60,7 +60,7 @@
                             <x-utils.tables.body>{{ $postulante->escuela->facultad->abrev }}</x-utils.tables.body>
                             <x-utils.tables.body>
                                 <x-utils.buttons.danger class="text-sm"
-                                                        wire:click="eliminar({{ $postulante->id }})">
+                                                        onclick="eliminar({{ $postulante->id }},{{$i+1}})">
                                     <x-icons.delete class="h-4 w-4" stroke="1.5"/>
                                 </x-utils.buttons.danger>
                             </x-utils.tables.body>
@@ -87,4 +87,17 @@
             </x-utils.links.primary>
         </x-utils.message-no-items>
     @endif
+
+    @push('js')
+        <script>
+            function eliminar(id, row) {
+                let res = confirm('¿Desea eliminar el registro número (' + row + ') de Bolsa de Trabajo?')
+
+                if (res) {
+                    window.livewire.emit('eliminar', id);
+                }
+            }
+        </script>
+    @endpush
+
 </div>
