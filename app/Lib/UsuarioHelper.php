b56164286dd663e70972a28f41747f65f0f002e9
Frank Paucar <fcpaucar@gmail.com>
2022-06-14 12:38:43 -0500

    feat: agrega la clase UsuarioHelper para usar funciones comunes

---
+++ app/Lib/UsuarioHelper.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Lib;
+
+use App\Models\Entidadable;
+use App\Models\Escuela;
+use Illuminate\Support\Facades\Auth;
+
+class UsuarioHelper
+{
+    public static function escuelasDelUsuario()
+    {
+        $entidades = Auth::user()->entidades->pluck('id');
+
+        $callback = function ($query) use ($entidades) {
+            $query->whereIn('id', $entidades);
+        };
+
+        $entidad_escuela = Entidadable::query()
+            ->where('entidadable_type', "App\\Models\\Escuela")
+            ->whereHas('entidad', $callback)
+            ->pluck('entidadable_id');
+
+        $entidad_facultad = Entidadable::query()
+            ->where('entidadable_type', "App\\Models\\Facultad")
+            ->whereHas('entidad', $callback)
+            ->pluck('entidadable_id');
+
+        if (count($entidad_facultad)) {
+            return Escuela::whereIn('facultad_id', $entidad_facultad)
+                ->orderBy('nombre')->get();
+        } else {
+            return Escuela::whereIn('id', $entidad_escuela)
+                ->orderBy('nombre')->get();
+        }
+    }
+}
