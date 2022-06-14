d0b57c39347ece6d80049ac9386ed9036fec446f
Frank Paucar <fcpaucar@gmail.com>
2022-06-13 22:10:20 -0500

    refactor: cambia los estilos de rounded y whitespace

--- resources/views/components/utils/badge.blade.php
+++ resources/views/components/utils/badge.blade.php
@@ -1,3 +1,3 @@
-<span {{ $attributes->merge(['class' => 'px-2 py-1 text-sm inline-flex rounded-lg'])}}>
+<span {{ $attributes->merge(['class' => 'px-2 py-1 text-sm inline-flex rounded-md whitespace-nowrap'])}}>
     {{ $slot }}
 </span>
