05ab4035ef2ebd8e784fae63a76b2271f5dfdfab
Frank Paucar <fcpaucar@gmail.com>
2022-06-13 22:09:47 -0500

    feat: agrega una nueva componente para los mensajes 403

---
+++ resources/views/components/utils/unauth.blade.php
@@ -0,0 +1,14 @@
+<section class="border rounded md:rounded-lg px-2 py-3 md:px-4 md:py-5 lg:px-6 lg:py-7">
+    <img src="{{ asset('images/svg/auth.svg') }}"
+         alt="Grafico que representa el acceso No Autorizado"
+         class="block w-3/5 sm:w-2/5 md:w-1/5 mx-auto">
+
+    <div class="mt-4 text-center">
+        <h3 class="text-base lg:text-lg font-bold text-gray-800 mb-3">
+            ¡Parece que no estás autorizado para ver esta página!
+        </h3>
+        <a href="/" class="text-sm font-semibold text-sky-600 hover:text-sky-800 hover:underline soft-transition">
+            🏠 Volver al inicio
+        </a>
+    </div>
+</section>
