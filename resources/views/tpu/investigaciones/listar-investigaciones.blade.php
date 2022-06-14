63220ae75b3ae3fd1a40efe94b7a4e3e3d209137
Angel Pagola <57104299+angelpagola@users.noreply.github.com>
2022-06-14 14:56:29 -0500

    fix: cambia el ancho de las cajas en proyectos de investigación.

--- resources/views/tpu/investigaciones/listar-investigaciones.blade.php
+++ resources/views/tpu/investigaciones/listar-investigaciones.blade.php
@@ -1,5 +1,5 @@
 <x-app-layout>
-    <div class="grid grid-cols-6 gap-12 pt-8">
+    <div class="grid grid-cols-8 gap-12 pt-8">
         <div class="col-span-2 space-y-4">
             <div class="text-right">
                 <p class="text-gray-500">Proyectos de </p>
@@ -14,7 +14,7 @@
             </div>
         </div>
 
-        <div class="col-span-4 space-y-6">
+        <div class="col-span-6 space-y-6">
             <livewire:tpu.investigaciones.listar-investigaciones :escuela="$escuela" :facultad="$facultad"/>
         </div>
     </div>
