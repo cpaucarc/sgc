caf60cc619a756f6973a62d410f1d9365c28fb37
Frank Paucar <fcpaucar@gmail.com>
2022-06-14 12:45:26 -0500

    refactor: remueve la variable intermedia para el uso del DNI del usuario

--- app/Http/Livewire/Investigacion/ListaMisInvestigaciones.php
+++ app/Http/Livewire/Investigacion/ListaMisInvestigaciones.php
@@ -9,23 +9,20 @@ use Livewire\Component;
 class ListaMisInvestigaciones extends Component
 {
     public $investigaciones = null;
-    public $investigador_dni = null;
-
-    public function mount()
-    {
-        $this->investigador_dni = Auth::user()->dni;
-    }
+    public $search = "";
 
     public function render()
     {
         $this->investigaciones = Investigacion::query()
             ->select('id', 'uuid', 'titulo', 'fecha_publicacion', 'escuela_id', 'estado_id', 'created_at')
+            ->where('titulo', 'like', '%' . $this->search . '%')
             ->with('escuela:id,nombre', 'estado:id,nombre,color')
             ->withCount('investigadores')
             ->withSum('financiaciones as presupuesto_sum', 'investigacion_financiacion.presupuesto')
             ->whereHas('investigadores', function ($query) {
-                $query->where("dni_investigador", $this->investigador_dni);
+                $query->where("dni_investigador", Auth::user()->dni);
             })
+            ->orderBy('created_at', 'desc')
             ->get();
 
         return view('livewire.investigacion.lista-mis-investigaciones');
