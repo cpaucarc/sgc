a917fdf31370b74496ee402041d665b2dfdb9c12
Frank Paucar <fcpaucar@gmail.com>
2022-06-13 19:22:02 -0500

    refactor: corrige la escritura de las palabras de los indicadores

--- resources/views/components/utils/links/external.blade.php
+++ resources/views/components/utils/links/external.blade.php
@@ -1,6 +1,6 @@
 <a href="{{ $to }}"
    target="_blank"
-   class="group flex items-center gap-x-2 px-8 py-4 text-lg rounded font-semibold bg-white text-gray-700 hover:text-white hover:bg-indigo-600 transition ease-in-out duration-300"
+   class="group flex items-center gap-x-2 px-8 py-4 text-lg rounded font-semibold bg-white text-gray-700 hover:text-white hover:bg-indigo-600 soft-transition"
    rel="noreferrer nofollow">
     @if(isset($img))
         <img src="{{ $img }}" class="h-8 overflow-hidden flex-shrink-0 object-cover" alt="Icono">
