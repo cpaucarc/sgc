b552183a51b11f34bcb566d0b80b38c8cec34261
Frank Paucar <fcpaucar@gmail.com>
2022-06-15 17:52:34 -0500

    :seedling: aumenta la cantidad de roles en el seeder

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Role;
 
@@ -15,19 +14,29 @@ class RoleSeeder extends Seeder
      */
     public function run()
     {
-        Role::create(['name' => 'Dirección de Escuela']);
-        Role::create(['name' => 'Departamento Academico']);
-        Role::create(['name' => 'Oficina General de Estudios']);
-        Role::create(['name' => 'Docente']);
-        Role::create(['name' => 'Decanatura']);
-        Role::create(['name' => 'Direccion de Unidad de Calidad']);
-        Role::create(['name' => 'Biblioteca']);
-        Role::create(['name' => 'Comite de Tutoria']);
-        Role::create(['name' => 'Estudiante']);
-        Role::create(['name' => 'Vicerrectorado Académico']);
-        Role::create(['name' => 'Vicerrectorado de Investigación']);
-        Role::create(['name' => 'Direccion de Unidad de Responsabilidad Social']);
-        Role::create(['name' => 'Direccion de Unidad de Investigación']);
+        Role::create(['name' => 'Dirección de Escuela']);  //id:1
+        Role::create(['name' => 'Departamento Academico']); //id:2
+        Role::create(['name' => 'Oficina General de Estudios']); //id:3
+        Role::create(['name' => 'Docente']); //id:4
+        Role::create(['name' => 'Decanatura']); //id:5
+        Role::create(['name' => 'Direccion de Unidad de Calidad']); //id:6
+        Role::create(['name' => 'Biblioteca']); //id:7
+        Role::create(['name' => 'Comite de Tutoria']); //id:8
+        Role::create(['name' => 'Estudiante']); //id:9
+        Role::create(['name' => 'Vicerrectorado Académico']); //id:10
+        Role::create(['name' => 'Vicerrectorado de Investigación']); //id:11
+        Role::create(['name' => 'Direccion de Unidad de Responsabilidad Social']); //id:12
+        Role::create(['name' => 'Direccion de Unidad de Investigación']); //id:13
+        Role::create(['name' => 'Consejo de Facultad']); //id:14
+        Role::create(['name' => 'Oficina General de Calidad Universitaria']); //id:15
+        Role::create(['name' => 'Dirección de Evaluación y Acreditación']); //id:16
+        Role::create(['name' => 'Rectorado']); //id:17
+        Role::create(['name' => 'Comité de Calidad']); //id:18
+        Role::create(['name' => 'Dirección General de Administración']); //id:19
+        Role::create(['name' => 'Dirección de Abastecimiento y Servicios Auxiliares']); //id:20
+        Role::create(['name' => 'Entidad Evaluadora Externa']); //id:21
+        Role::create(['name' => 'Consejo Universitario']); //id:22
+        Role::create(['name' => 'Plan de Estudios']); //id:23
         Role::create(['name' => 'Administrador']);
     }
 }
