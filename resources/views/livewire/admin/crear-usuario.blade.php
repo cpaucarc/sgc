bb8d6dc7775b41438616ef1042801317a17a8342
Frank Paucar <fcpaucar@gmail.com>
2022-06-15 12:49:51 -0500

    :art: reemplaza el icono de search por spinner al iniciar la busqueda por DNI

--- resources/views/livewire/admin/crear-usuario.blade.php
+++ resources/views/livewire/admin/crear-usuario.blade.php
@@ -19,7 +19,14 @@
                         <x-jet-input id="dni" autocomplete="off" maxlength="8" class="flex-1" type="text"
                                      wire:model.defer="dni"/>
                         <x-utils.buttons.default wire:click="buscar">
-                            <x-icons.search class="w-4 h-4"/>
+                            <x-icons.search wire:loading.remove wire:target="buscar" class="icon-4"/>
+                            <svg wire:loading wire:target="buscar" class="animate-spin icon-4 text-sky-600"
+                                 fill="none" viewBox="0 0 24 24">
+                                <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor"
+                                        stroke-width="4"></circle>
+                                <path class="opacity-75" fill="currentColor"
+                                      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                            </svg>
                         </x-utils.buttons.default>
                     </div>
                     <x-jet-input-error for="dni"/>
