7e78f23187c26ac0d4b7690e4e477a74d17a3abe
Frank Paucar <fcpaucar@gmail.com>
2022-06-16 15:33:27 -0500

    :recycle: agrega los timestamp en la relacion entre usuario y entidades

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -33,6 +32,7 @@ class User extends Authenticatable
     public function entidades()
     {
         return $this->belongsToMany(Entidad::class, 'entidad_user')
+            ->withTimestamps()
             ->withPivot('id');
     }
 
