a700fb44c1126f06ee002386427055d7350a88d6
Frank Paucar <fcpaucar@gmail.com>
2022-06-16 15:34:35 -0500

    :art: cambia el color el checkbox cuando esta en el estado de desactivado

--- resources/views/components/utils/forms/checkbox.blade.php
+++ resources/views/components/utils/forms/checkbox.blade.php
@@ -1,7 +1,7 @@
 <input
     {{ $attributes->merge([
         'type' => 'checkbox',
-        'class' => 'mr-2 cursor-pointer rounded border border-gray-300 text-indigo-600 focus:ring-0 focus:ring-white disabled:bg-gray-50 disabled:cursor-not-allowed'
+        'class' => 'mr-2 cursor-pointer rounded border border-gray-300 text-indigo-600 focus:ring-0 focus:ring-white disabled:checked:bg-indigo-400 disabled:cursor-not-allowed soft-transition'
     ])}}
 />
 
