70cb7fa64f873a814765fa23f3b1fb9f4563bf76
Frank Paucar <fcpaucar@gmail.com>
2022-06-17 11:43:24 -0500

    :recycle: implementa el uso de componentes para añadir estudiantes y docentes

--- app/Http/Livewire/Rsu/Participantes.php
+++ app/Http/Livewire/Rsu/Participantes.php
@@ -5,7 +5,9 @@ namespace App\Http\Livewire\Rsu;
 use App\Models\Oge;
 use App\Models\ResponsabilidadSocial;
 use App\Models\RsuParticipante;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 use Livewire\Component;
 
 class Participantes extends Component
@@ -13,18 +15,18 @@ class Participantes extends Component
     public $rsu_id;
     public $es_responsable;
     public $en_docentes = false; // Variable para saber en que API buscar, y que vista mostrar (Estudiantes, Docentes)
-
     public $open = false; // Abrir modal de datos del participante
     public $datos_participante = null;
+    public $add = false; // Abrir modal para agregar participantes
 
-    public $add = true; // Abrir modal para agregar participantes
-    public $mensaje_estudiantes = null;
-    public $dni = ""; // DNI ingresados al input
+    protected $listeners = ["render"];
 
     public function mount($rsu_id, $es_responsable)
     {
         $this->rsu_id = $rsu_id;
         $this->es_responsable = $es_responsable;
+        $this->emitTo("rsu.agregar-participante-docente", "cargarDocentes");
+        Log::info('montando datos', [$rsu_id]);
     }
 
     public function render()
@@ -52,82 +54,9 @@ class Participantes extends Component
 
     public function buscarEnDocentes($es_docente)
     {
-        $this->en_docentes = $es_docente;
+        $this->en_docentes = $es_docente; // Para cambiar de pestaña entre 'Añadir estudiantes' y 'Añadir docentes'
+        if ($es_docente)
+            $this->emitTo("rsu.agregar-participante-docente", "cargarDocentes");
     }
 
-    public function agregarEstudiantes()
-    {
-        try {
-            if (strlen($this->dni) === 0) {
-                $this->mensaje_estudiantes = "Ingrese al menos el DNI de un estudiante";
-                $this->emit('error', "Ingrese al menos el DNI de un estudiante");
-                return;
-            }
-
-            if (strlen($this->dni) < 8) {
-                $this->mensaje_estudiantes = "Parece que el DNI ingresado tiene menos de 8 digitos";
-                $this->emit('error', "Parece que el DNI ingresado tiene menos de 8 digitos");
-                return;
-            }
-
-            $arrayDni = explode(",", $this->dni);
-            $arrayDniIncorrectos = array();
-            foreach ($arrayDni as $d) {
-                if (!is_numeric($d)) {
-                    $arrayDniIncorrectos[] = "♦ '" . $d . "' debería contener solo números.";
-                    continue;
-                }
-
-                if (strlen($d) !== 8)
-                    $arrayDniIncorrectos[] = "► '" . $d . "' debería contener 8 caracteres.";
-            }
-
-            if (count($arrayDniIncorrectos)) {
-                $this->mensaje_estudiantes = implode("\r\n", $arrayDniIncorrectos);
-                $this->emit('error', implode("\r\n", $arrayDniIncorrectos));
-                return;
-            }
-
-            $arrayDniExistente = array(); // array de DNI que si existen en OGE
-            $arrayDniInexistente = array(); // array de DNI que no se han encontrado
-            foreach ($arrayDni as $d) {
-                $response = Http::withToken(env('OGE_TOKEN'))
-                    ->get(env('OGE_API') . 'ensenianza_aprendizaje/01?codigo=' . $d);
-                $response = $response->json();
-
-                if (isset($response['dni'])) {
-                    $arrayDniExistente[] = [
-                        'fecha_incorporacion' => now()->format('Y-m-d'),
-                        'es_responsable' => false,
-                        'es_estudiante' => true,
-                        'dni_participante' => $d,
-                        'responsabilidad_social_id' => $this->rsu_id
-                    ];
-                } else {
-                    $arrayDniInexistente[] = "• No encontramos ningún estudiante con DNI " . $d . ", revise si está bien escrito.";
-                }
-            }
-
-            // si hay elementos en $arrayDniExistente, guardar en BD
-            if (count($arrayDniExistente)) {
-                RsuParticipante::insert($arrayDniExistente);
-                $this->emit('guardado', "Se añadieron correctamente " . count($arrayDniExistente) . " de " . count($arrayDni) . " nuevos participantes.");
-                $this->mensaje_estudiantes = null;
-            }
-
-            // si hay elementos en $arrayDniInexistente, mostrar en el msg
-            if (count($arrayDniInexistente)) {
-                $this->mensaje_estudiantes = implode("\r\n", $arrayDniInexistente);
-            }
-
-            // si hay elementos en $arrayDniInexistente, mantener abierto modal
-
-            // si no hay elementos en $arrayDniInexistente, cerrar modal
-//            if (!count($arrayDniInexistente)) {
-//                $this->add = false;
-//            }
-        } catch (\Exception $e) {
-            $this->emit('error', "Hubo un error inesperado\n" . $e);
-        }
-    }
 }
--- resources/views/livewire/rsu/participantes.blade.php
+++ resources/views/livewire/rsu/participantes.blade.php
@@ -1,4 +1,4 @@
-<div class="space-y-2">
+<div class="space-y-2" xmlns="http://www.w3.org/1999/html">
 
     <div class="flex justify-between items-center">
         <h3 class="font-bold tracking-wide text-gray-600">
@@ -116,38 +116,11 @@
                     </button>
                 </div>
 
-                <div class="border rounded-md p-4">
-                    @if($en_docentes)
-                        docentes
-                    @else
-                        <div class="space-y-4">
-                            <div>
-                                <x-jet-label for="dni" value="DNI de los estudiantes (separados por comas)"/>
-                                <div class="flex gap-x-2">
-                                    <x-jet-input id="dni" autocomplete="off" class="flex-1" type="text"
-                                                 placeholder="74125896,77788955,..." wire:model.defer="dni"/>
-                                    <x-utils.buttons.default wire:click="agregarEstudiantes">
-                                        <x-icons.search wire:loading.remove wire:target="agregarEstudiantes"
-                                                        class="icon-4"/>
-                                        <svg wire:loading wire:target="agregarEstudiantes"
-                                             class="animate-spin icon-4 text-sky-600"
-                                             fill="none" viewBox="0 0 24 24">
-                                            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor"
-                                                    stroke-width="4"></circle>
-                                            <path class="opacity-75" fill="currentColor"
-                                                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-                                        </svg>
-                                        <span class="ml-2 font-normal">Buscar y guardar</span>
-                                    </x-utils.buttons.default>
-                                </div>
-                                @if($mensaje_estudiantes)
-                                    <x-utils.alert.error-box>{{ $mensaje_estudiantes }}</x-utils.alert.error-box>
-                                @endif
-                            </div>
-                        </div>
-                    @endif
-                </div>
-
+                @if($en_docentes)
+                    <livewire:rsu.agregar-participante-docente :rsu_id="$rsu_id"/>
+                @else
+                    <livewire:rsu.agregar-participante-estudiante :rsu_id="$rsu_id"/>
+                @endif
             </div>
         </x-slot>
     </x-jet-dialog-modal>
@@ -171,5 +144,4 @@
             });
         </script>
     @endpush
-
 </div>
