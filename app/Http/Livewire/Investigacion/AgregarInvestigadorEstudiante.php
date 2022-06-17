96415ec6bd19b7cad728f233b177a0a26114b0b4
Frank Paucar <fcpaucar@gmail.com>
2022-06-17 13:35:05 -0500

    :sparkles: crea el componente para agregar estudiantes como investigadores

---
+++ app/Http/Livewire/Investigacion/AgregarInvestigadorEstudiante.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace App\Http\Livewire\Investigacion;
+
+use App\Models\Investigacion;
+use App\Models\InvestigacionInvestigador;
+use App\Models\Investigador;
+use App\Models\ResponsabilidadSocial;
+use App\Models\RsuParticipante;
+use Illuminate\Support\Facades\Http;
+use Livewire\Component;
+
+class AgregarInvestigadorEstudiante extends Component
+{
+
+    public $investigacion_id;
+    public $mensaje_estudiantes = null;
+    public $dni = ""; // DNI de estudiantes ingresados al input
+    public $dni_usados; // DNI de los estudiantes que ya estan registrados como investigadores
+
+    public function mount($investigacion_id)
+    {
+        $this->investigacion_id = $investigacion_id;
+        $investigaciones = Investigacion::query()
+            ->select('id')
+            ->with(['investigadores' => function ($query) {
+                $query->where('es_docente', false);
+            }])
+            ->where('id', $this->investigacion_id)
+            ->first();
+        $this->dni_usados = $investigaciones->investigadores->pluck('dni_investigador')->toArray();
+    }
+
+    public function render()
+    {
+        return view('livewire.investigacion.agregar-investigador-estudiante');
+    }
+
+    public function agregarEstudiantes()
+    {
+        try {
+            if (strlen($this->dni) === 0) {
+                $this->mensaje_estudiantes = "Ingrese al menos el DNI de un estudiante";
+                $this->emit('error', "Ingrese al menos el DNI de un estudiante");
+                return;
+            }
+
+            if (strlen($this->dni) < 8) {
+                $this->mensaje_estudiantes = "Parece que el DNI ingresado tiene menos de 8 digitos";
+                $this->emit('error', "Parece que el DNI ingresado tiene menos de 8 digitos");
+                return;
+            }
+
+            $arrayDni = explode(",", $this->dni);
+            $arrayDniIncorrectos = array();
+            foreach ($arrayDni as $d) {
+                if (!is_numeric($d)) {
+                    $arrayDniIncorrectos[] = "♦ '" . $d . "' debería contener solo números.";
+                    continue;
+                }
+
+                if (strlen($d) !== 8)
+                    $arrayDniIncorrectos[] = "► '" . $d . "' debería contener 8 caracteres.";
+
+                if (in_array($d, $this->dni_usados))
+                    $arrayDniIncorrectos[] = "▼ '" . $d . "' ya está registrado como investigador.";
+            }
+
+            if (count($arrayDniIncorrectos)) {
+                $this->mensaje_estudiantes = implode("\r\n", $arrayDniIncorrectos);
+                $this->emit('error', implode("\r\n", $arrayDniIncorrectos));
+                return;
+            }
+
+            $arrayDniExistente = array(); // array de DNI que si existen en OGE
+            $arrayDniInexistente = array(); // array de DNI que no se han encontrado
+            foreach ($arrayDni as $d) {
+                $response = Http::withToken(env('OGE_TOKEN'))
+                    ->get(env('OGE_API') . 'ensenianza_aprendizaje/01?codigo=' . $d);
+                $response = $response->json();
+
+                if (isset($response['dni'])) {
+
+                    $investigador = Investigador::query()->where('dni_investigador', $d)->first();
+
+                    if (is_null($investigador))
+                        $investigador = Investigador::create(['es_docente' => false, 'dni_investigador' => $d]);
+
+                    $arrayDniExistente[] = [
+                        'es_responsable' => false,
+                        'investigador_id' => $investigador->id,
+                        'investigacion_id' => $this->investigacion_id
+                    ];
+                    $this->dni_usados[] = $d;
+                } else {
+                    $arrayDniInexistente[] = "• No encontramos ningún estudiante con DNI " . $d . ", revise si está bien escrito.";
+                }
+            }
+
+            // si hay elementos en $arrayDniExistente, guardar en BD
+            if (count($arrayDniExistente)) {
+                InvestigacionInvestigador::insert($arrayDniExistente);
+                $this->emit('guardado', "Se añadieron correctamente " . count($arrayDniExistente) . " de " . count($arrayDni) . " nuevos investigadores.");
+                $this->emitTo("investigacion.lista-investigacion-participantes", "render");
+                $this->mensaje_estudiantes = null;
+            }
+
+            // si hay elementos en $arrayDniInexistente, mostrar en el msg
+            if (count($arrayDniInexistente)) {
+                $this->mensaje_estudiantes = implode("\r\n", $arrayDniInexistente);
+            }
+        } catch (\Exception $e) {
+            $this->emit('error', "Hubo un error inesperado\n" . $e);
+        }
+    }
+}
---
+++ resources/views/livewire/investigacion/agregar-investigador-estudiante.blade.php
@@ -0,0 +1,24 @@
+<div class="space-y-4">
+    <x-jet-label for="dni" value="DNI de los estudiantes (separados por comas)"/>
+    <div class="flex gap-x-2">
+        <x-jet-input id="dni" autocomplete="off" class="flex-1" type="text"
+                     placeholder="74125896,77788955,..." wire:model.defer="dni"/>
+        <x-jet-button wire:click="agregarEstudiantes">
+            <x-icons.search wire:loading.remove wire:target="agregarEstudiantes"
+                            class="icon-4 text-white"/>
+            <svg wire:loading wire:target="agregarEstudiantes"
+                 class="animate-spin icon-4 text-white"
+                 fill="none" viewBox="0 0 24 24">
+                <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor"
+                        stroke-width="4"></circle>
+                <path class="opacity-75" fill="currentColor"
+                      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+            </svg>
+            <span class="ml-2 font-normal">Buscar y guardar</span>
+        </x-jet-button>
+    </div>
+
+    @if($mensaje_estudiantes)
+        <x-utils.alert.error-box>{{ $mensaje_estudiantes }}</x-utils.alert.error-box>
+    @endif
+</div>
