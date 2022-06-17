73c0884fb2b395d9904fbad306025548097d8a22
Frank Paucar <fcpaucar@gmail.com>
2022-06-16 22:08:53 -0500

    :sparkles: agrega el componente error-box para mostrar mensajes de error

---
+++ resources/views/components/utils/alert/error-box.blade.php
@@ -0,0 +1,5 @@
+<div
+    class="mt-4 px-3 py-2 rounded-md rounded-md flex items-start gap-x-3 text-sm font-semibold text-rose-600 bg-rose-100">
+    <x-icons.warning class="icon-5"/>
+    {{ $slot }}
+</div>
