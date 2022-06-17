aec8e621960e4e571cc6e8d807a4690ac046708a
Frank Paucar <fcpaucar@gmail.com>
2022-06-16 22:09:44 -0500

    :art: agrega la clase mb-1 a los labeles de los input

--- resources/views/vendor/jetstream/components/label.blade.php
+++ resources/views/vendor/jetstream/components/label.blade.php
@@ -1,6 +1,6 @@
 @props(['value'])
 
 <label {{ $attributes->merge([
-    'class' => 'block font-medium text-sm text-gray-600']) }}>
+    'class' => 'block font-medium text-sm text-gray-600 mb-1']) }}>
     {{ $value ?? $slot }}
 </label>
