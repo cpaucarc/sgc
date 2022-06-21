b8aa97d6cfcd59b9a8be8f7d2dea6241020df44a
Angel Pagola <57104299+angelpagola@users.noreply.github.com>
2022-06-20 19:03:30 -0500

    :sparkles: Agrega eliminar una empresa si es usuario creador

--- app/Http/Livewire/Rsu/ListaEmpresaGeneral.php
+++ app/Http/Livewire/Rsu/ListaEmpresaGeneral.php
@@ -10,10 +10,39 @@ class ListaEmpresaGeneral extends Component
 {
     use WithPagination;
 
+    public $search = "";
+
+    public $listeners = ['render', 'eliminar'];
+
     public function render()
     {
-        $empresas = Empresa::query()->paginate(10);
+        $empresas = Empresa::query()
+            ->where(function ($query) {
+                $query->where('nombre', 'like', '%' . $this->search . '%')
+                    ->orWhere('ruc', 'like', '%' . $this->search . '%')
+                    ->orWhere('ubicacion', 'like', '%' . $this->search . '%');
+            })
+            ->orderBy('created_at', 'desc')
+            ->paginate(15);
 
         return view('livewire.rsu.lista-empresa-general', compact('empresas'));
+    }
+
+    /* Funciones */
+
+    public function eliminar($id)
+    {
+        $dependientes = Empresa::query()
+            ->whereIn('id', function ($query) use ($id) {
+                $query->select('empresa_id')->from('responsabilidad_social')
+                    ->where('empresa_id', $id);
+            })
+            ->count();
+
+        if ($dependientes > 0) {
+            $this->emit('error', 'No es posible eliminar esta empresa porque se encuentra en uso en Responsabilidad Social.');
+        } else {
+            Empresa::find($id)->delete();
+        }
     }
 }
--- resources/views/livewire/rsu/lista-empresa-general.blade.php
+++ resources/views/livewire/rsu/lista-empresa-general.blade.php
@@ -15,6 +15,10 @@
             </div>
         </div>
     </x-utils.card>
+
+    <div class="flex justify-between mt-4">
+        <x-utils.forms.search-input wire:model.debounce.500ms="search"/>
+    </div>
     @if(count($empresas)>0)
 
         <div class="py-4">
@@ -40,10 +44,12 @@
                             <x-utils.tables.body>{{ $empresa->direccion }}</x-utils.tables.body>
                             <x-utils.tables.body>{{ $empresa->ubicacion }}</x-utils.tables.body>
                             <x-utils.tables.body>
-                                {{-- <x-utils.buttons.danger class="text-sm"
-                                                         onclick="eliminar({{ $empresa->id }},{{($i+1)}})">
-                                     <x-icons.delete class="h-4 w-4" stroke="1.5"/>
-                                 </x-utils.buttons.danger>--}}
+                                @if($empresa->user_id === \Illuminate\Support\Facades\Auth::user()->id)
+                                    <x-utils.buttons.danger class="text-sm"
+                                                            onclick="eliminar({{ $empresa->id }},'{{ $empresa->nombre }}')">
+                                        <x-icons.delete class="h-4 w-4" stroke="1.5"/>
+                                    </x-utils.buttons.danger>
+                                @endif
                             </x-utils.tables.body>
                         </x-utils.tables.row>
                     @endforeach
@@ -55,16 +61,15 @@
             </div>
         </div>
     @else
-        <div class="w-full border border-gray-300 rounded-md">
+        <div class="w-full border border-gray-300 rounded-md mt-4">
             <x-utils.message-no-items
                 title="Aún no hay datos sobre empresas"
                 text="Aquí podrá encontrar la información de empresas para la Responsabilidad Social.">
                 @slot('icon')
-                    <svg class="text-gray-400" fill="currentColor" viewBox="0 0 24 24" width="24" height="24">
-                        <path fill-rule="evenodd"
-                              d="M3.5 8a5.5 5.5 0 118.596 4.547 9.005 9.005 0 015.9 8.18.75.75 0 01-1.5.045 7.5 7.5 0 00-14.993 0 .75.75 0 01-1.499-.044 9.005 9.005 0 015.9-8.181A5.494 5.494 0 013.5 8zM9 4a4 4 0 100 8 4 4 0 000-8z"></path>
-                        <path
-                            d="M17.29 8c-.148 0-.292.01-.434.03a.75.75 0 11-.212-1.484 4.53 4.53 0 013.38 8.097 6.69 6.69 0 013.956 6.107.75.75 0 01-1.5 0 5.193 5.193 0 00-3.696-4.972l-.534-.16v-1.676l.41-.209A3.03 3.03 0 0017.29 8z"></path>
+                    <svg class="text-gray-400" width="24" height="24" fill="none" viewBox="0 0 24 24"
+                         stroke="currentColor" stroke-width="1.5">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                              d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4"/>
                     </svg>
                 @endslot
                 <x-utils.links.primary class="text-sm" href="{{ route('rsu.business.create') }}">
@@ -74,4 +79,28 @@
         </div>
     @endif
 
+    @push('js')
+        {{--<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>--}}
+        <script>
+            function eliminar(id, nombre) {
+                let res = confirm('¿Desea eliminar la empresa ' + nombre + '?')
+
+                if (res) {
+                    window.livewire.emit('eliminar', id);
+                }
+            }
+
+            Livewire.on('error', function eliminar(msg) {
+                alert(msg)
+            });
+
+            /*Livewire.on('error', msg => {
+                Swal.fire({
+                    icon: 'error',
+                    title: '',
+                    text: msg,
+                });
+            });*/
+        </script>
+    @endpush
 </div>
