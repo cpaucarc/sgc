3772cfac2d9efbd9082cc81803db39f9b1fed1c0
Frank Paucar <fcpaucar@gmail.com>
2022-07-05 11:55:43 -0500

    :card_file_box: agrega la tabla personas

---
+++ database/migrations/2022_07_05_111645_create_personas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('personas', function (Blueprint $table) {
+            $table->id();
+            $table->string('apellido_paterno');
+            $table->string('apellido_materno');
+            $table->string('nombres');
+            $table->string('dni', 8)->unique();
+            $table->string('correo')->unique()->nullable();
+            $table->string('celular')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('personas');
+    }
+};
