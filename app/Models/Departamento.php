ba499b326f73c78cbb813ec7186fd297e61b70fe
Frank Paucar <fcpaucar@gmail.com>
2022-07-05 19:46:34 -0500

    :sparkles: agrega la relacion con facultad

--- app/Models/Departamento.php
+++ app/Models/Departamento.php
@@ -8,6 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Departamento extends Model
 {
     use HasFactory;
+
     public $timestamps = false;
     public $fillable = ['nombre', 'facultad_id'];
+
+    public function facultad()
+    {
+        return $this->belongsTo(Facultad::class);
+    }
 }
