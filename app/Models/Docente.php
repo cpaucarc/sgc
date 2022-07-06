6c3feb97da800186a662d57cad26685884d19da7
Frank Paucar <fcpaucar@gmail.com>
2022-07-05 19:47:04 -0500

    :sparkles: agrega las relaciones con persona, categoria, condicion y dedicacion

--- app/Models/Docente.php
+++ app/Models/Docente.php
@@ -11,4 +11,24 @@ class Docente extends Model
 
     public $timestamps = false;
     public $fillable = ['grado', 'persona_id', 'departamento_id', 'categoria_id', 'condicion_id', 'dedicacion_id'];
+
+    public function persona()
+    {
+        return $this->belongsTo(Persona::class);
+    }
+
+    public function categoria()
+    {
+        return $this->belongsTo(DocenteCategoria::class, 'categoria_id');
+    }
+
+    public function condicion()
+    {
+        return $this->belongsTo(DocenteCondicion::class, 'condicion_id');
+    }
+
+    public function dedicacion()
+    {
+        return $this->belongsTo(DocenteDedicacion::class, 'dedicacion_id');
+    }
 }
