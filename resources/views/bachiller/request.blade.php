67c856a6d36dddea4adb6804d2d0cf2d5c1fde0c
Angel Pagola <57104299+angelpagola@users.noreply.github.com>
2022-07-08 15:07:01 -0500

    :art:  Une bachiller y tpu en una sola ruta para estudiantes

--- resources/views/bachiller/request.blade.php
+++ resources/views/bachiller/request.blade.php
@@ -1,33 +1,46 @@
 <x-app-layout>
-    <x-utils.card class="mb-4">
-        <livewire:bachiller.solicitud.estado-solicitud/>
-    </x-utils.card>
-    <div class="grid grid-cols-6 gap-4 flex items-start justify-between">
-        <x-utils.card class="col-span-4">
-            @slot('header')
-                <div class="flex justify-between items-center space-x-2">
-                    <div class="pr-4 flex-1">
-                        <h1 class="text-xl font-bold text-gray-800">
-                            Requisitos enviados
-                        </h1>
-                    </div>
-                    <livewire:bachiller.solicitud.enviar-requisito/>
-                </div>
-            @endslot
-            <livewire:bachiller.solicitud.requisitos-enviados/>
-        </x-utils.card>
+    <div class="grid grid-cols-4 gap-x-8">
+        {{-- Rutas --}}
+        <div class="col-span-1">
+            <x-bachiller.rutas-grado-academico/>
+        </div>
 
-        <x-utils.card class="col-span-2">
-            @slot('header')
-                <div class="flex justify-between items-center space-x-2">
-                    <div class="pr-4 flex-1">
-                        <h1 class="text-xl font-bold text-gray-800">
-                            Requisitos faltantes
-                        </h1>
-                    </div>
-                </div>
-            @endslot
-            <livewire:bachiller.solicitud.lista-requisitos/>
-        </x-utils.card>
+        <div class="col-span-3 space-y-4">
+
+            <x-utils.card class="mb-4">
+                <livewire:bachiller.solicitud.estado-solicitud/>
+            </x-utils.card>
+            <div class="grid grid-cols-6 gap-4 flex items-start justify-between">
+                <x-utils.card class="col-span-4">
+                    @slot('header')
+                        <div class="flex justify-between items-center space-x-2">
+                            <div class="pr-4 flex-1">
+                                <h1 class="text-xl font-bold text-gray-800">
+                                    Requisitos enviados
+                                </h1>
+                            </div>
+                            <livewire:bachiller.solicitud.enviar-requisito/>
+                        </div>
+                    @endslot
+                    <livewire:bachiller.solicitud.requisitos-enviados/>
+                </x-utils.card>
+
+                <x-utils.card class="col-span-2">
+                    @slot('header')
+                        <div class="flex justify-between items-center space-x-2">
+                            <div class="pr-4 flex-1">
+                                <h1 class="text-xl font-bold text-gray-800">
+                                    Requisitos faltantes
+                                </h1>
+                            </div>
+                        </div>
+                    @endslot
+                    <livewire:bachiller.solicitud.lista-requisitos/>
+                </x-utils.card>
+            </div>
+
+        </div>
     </div>
+
+
 </x-app-layout>
--- resources/views/tpu/request.blade.php
+++ resources/views/tpu/request.blade.php
@@ -1,34 +1,47 @@
 <x-app-layout>
-    <x-utils.card class="mb-4">
-        <livewire:tpu.solicitud.estado-solicitud/>
-    </x-utils.card>
-    <div class="grid grid-cols-6 gap-4 flex items-start justify-between">
-        <x-utils.card class="col-span-4">
-            @slot('header')
-                <div class="flex justify-between items-center space-x-2">
-                    <div class="pr-4 flex-1">
-                        <h1 class="text-xl font-bold text-gray-800">
-                            Requisitos enviados
-                        </h1>
-                    </div>
-                    <livewire:tpu.solicitud.enviar-requisito/>
-                </div>
-            @endslot
-            <livewire:tpu.solicitud.requisitos-enviados/>
-        </x-utils.card>
+    <div class="grid grid-cols-4 gap-x-8">
+        {{-- Rutas --}}
+        <div class="col-span-1">
+            <x-bachiller.rutas-grado-academico/>
+        </div>
 
-        <x-utils.card class="col-span-2">
-            @slot('header')
-                <div class="flex justify-between items-center space-x-2">
-                    <div class="pr-4 flex-1">
-                        <h1 class="text-xl font-bold text-gray-800">
-                            Requisitos faltantes
-                        </h1>
-                    </div>
-                </div>
-            @endslot
-            <livewire:tpu.solicitud.lista-requisitos/>
-            <livewire:tpu.tesis.ir-tesis/>
-        </x-utils.card>
+        <div class="col-span-3 space-y-4">
+
+            <x-utils.card class="mb-4">
+                <livewire:tpu.solicitud.estado-solicitud/>
+            </x-utils.card>
+            <div class="grid grid-cols-6 gap-4 flex items-start justify-between">
+                <x-utils.card class="col-span-4">
+                    @slot('header')
+                        <div class="flex justify-between items-center space-x-2">
+                            <div class="pr-4 flex-1">
+                                <h1 class="text-xl font-bold text-gray-800">
+                                    Requisitos enviados
+                                </h1>
+                            </div>
+                            <livewire:tpu.solicitud.enviar-requisito/>
+                        </div>
+                    @endslot
+                    <livewire:tpu.solicitud.requisitos-enviados/>
+                </x-utils.card>
+
+                <x-utils.card class="col-span-2">
+                    @slot('header')
+                        <div class="flex justify-between items-center space-x-2">
+                            <div class="pr-4 flex-1">
+                                <h1 class="text-xl font-bold text-gray-800">
+                                    Requisitos faltantes
+                                </h1>
+                            </div>
+                        </div>
+                    @endslot
+                    <livewire:tpu.solicitud.lista-requisitos/>
+                    <livewire:tpu.tesis.ir-tesis/>
+                </x-utils.card>
+            </div>
+
+        </div>
     </div>
+
+
 </x-app-layout>
