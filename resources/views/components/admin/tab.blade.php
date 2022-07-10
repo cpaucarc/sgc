b8ad81fb001fd048d099c4712fb40a8bdb1be5b0
Frank Paucar <fcpaucar@gmail.com>
2022-07-09 20:53:40 -0500

    :lipstick: agrega el slot 'otros' para añadir elementos necesarios

--- resources/views/components/admin/tab.blade.php
+++ resources/views/components/admin/tab.blade.php
@@ -1,14 +1,17 @@
 @props(['active'])
 
 @php
-    $common_classes = "inline-flex pt-2 pb-2 px-6 text-sm text-center rounded-t-lg border-b-2 group soft-transition ";
+    $common_classes = "inline-flex pt-2 pb-2 px-4 text-sm text-center rounded-t-lg border-b-2 group soft-transition whitespace-nowrap ";
     $classes = ($active ?? false)
                 ? $common_classes . 'font-bold text-blue-600 border-blue-600 bg-blue-500/10 active'
                 : $common_classes . 'font-medium text-gray-500 border-zinc-100 hover:text-zinc-600 hover:border-zinc-300';
 @endphp
 
-<li>
+<li class="relative">
     <a {{ $attributes->merge(['class' => $classes]) }}>
         {{ $slot }}
     </a>
+    @if(isset($otros))
+        {{ $otros }}
+    @endif
 </li>
