f8165232347e5e98aab045b21fa7b86c8acf930a
Frank Paucar <fcpaucar@gmail.com>
2022-07-12 20:11:53 -0500

    :lipstick:  corrige el color de fondo y borde del boton primario

--- resources/views/components/utils/links/primary.blade.php
+++ resources/views/components/utils/links/primary.blade.php
@@ -1,3 +1,3 @@
-<a {{ $attributes->merge([ 'class' => 'btn text-sm text-white bg-sky-600 hover:bg-sky-700 active:bg-sky-600 border border-sky-600' ]) }}>
+<a {{ $attributes->merge([ 'class' => 'btn text-sm text-white bg-sky-600 hover:bg-sky-700 active:bg-sky-600 border border-sky-700' ]) }}>
     {{$slot}}
 </a>
--- resources/views/vendor/jetstream/components/button.blade.php
+++ resources/views/vendor/jetstream/components/button.blade.php
@@ -1,8 +1,7 @@
 <button {{ $attributes->merge([
     'type' => 'submit',
-    'class' => 'btn text-sm text-white bg-sky-500 hover:bg-sky-600 active:bg-sky-500 disabled:cursor-not-allowed disabled:bg-sky-900 disabled:bg-opacity-75'
+    'class' => 'btn text-sm text-white bg-sky-600 hover:bg-sky-700 active:bg-sky-600 border border-sky-700 disabled:cursor-not-allowed disabled:bg-sky-900 disabled:bg-opacity-75'
     ])
 }}>
     {{$slot}}
 </button>
-
