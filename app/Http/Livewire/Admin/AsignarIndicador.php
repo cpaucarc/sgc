7af074127769d70da6385cf8194395a08c95dfd2
Angel Pagola <57104299+angelpagola@users.noreply.github.com>
2022-07-28 15:37:15 -0500

    :sparkles: Agrega indicadores a indicadorable

--- app/Http/Livewire/Admin/AsignarIndicador.php
+++ app/Http/Livewire/Admin/AsignarIndicador.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Livewire\Admin;
 
+use App\Models\Escuela;
+use App\Models\Facultad;
 use App\Models\Indicador;
 use App\Models\Indicadorable;
 use Livewire\Component;
@@ -9,13 +11,20 @@ use Livewire\Component;
 class AsignarIndicador extends Component
 {
     public $indicador_id;
+    public $indicador = null;
+
     public $open = false;
     public $indicador_en_facultades = null, $ind_fac_actual = [];
     public $indicador_en_escuelas = null, $ind_esc_actual = [];
 
+    public $facultades_not_indicador = null, $facultades_selected = [];
+    public $escuelas_not_indicador = null, $escuelas_selected = [];
+
+
     public function mount($indicador_id)
     {
         $this->indicador_id = $indicador_id;
+        $this->indicador = Indicador::query()->where('id', $this->indicador_id)->first();
     }
 
     public function render()
@@ -24,14 +33,66 @@ class AsignarIndicador extends Component
             ->where('indicador_id', $this->indicador_id)
             ->where('indicadorable_type', 'App\Models\Facultad');
         $this->indicador_en_facultades = $query_fac->get();
-        $this->ind_fac_actual = $query_fac->pluck('id');
+        $this->ind_fac_actual = $query_fac->pluck('indicadorable_id');
 
         $query_esc = Indicadorable::query()
             ->where('indicador_id', $this->indicador_id)
             ->where('indicadorable_type', 'App\Models\Escuela');
         $this->indicador_en_escuelas = $query_esc->get();
-        $this->ind_esc_actual = $query_esc->pluck('id');
+        $this->ind_esc_actual = $query_esc->pluck('indicadorable_id');
 
         return view('livewire.admin.asignar-indicador');
+    }
+
+    /* Funciones */
+    public function openModal()
+    {
+        $this->open = true;
+        $this->facultades_not_indicador = Facultad::query()
+            ->whereNotIn('id', $this->ind_fac_actual)->get();
+    }
+
+    public function updatedFacultadesSelected()
+    {
+        if (count($this->facultades_selected) === 0) {
+            $this->escuelas_not_indicador = null;
+        } else {
+            $this->escuelas_not_indicador = Escuela::query()
+                ->whereIn('facultad_id', function ($query) {
+                    $query->select('id')->from('facultades')->whereIn('id', $this->facultades_selected);
+                })
+                ->whereNotIn('id', $this->ind_esc_actual)->get();
+        }
+    }
+
+    public function asignarIndicador()
+    {
+        if (count($this->facultades_selected) > 0) {
+            foreach ($this->facultades_selected as $facultad) {
+                $fac_abrev = Facultad::select('abrev')->where('id', $facultad)->first();
+                Indicadorable::create([
+                    'cod_ind_final' => ($this->indicador->cod_ind_inicial . '-' . $fac_abrev->abrev),
+                    'minimo' => $this->indicador->minimo,
+                    'sobresaliente' => $this->indicador->sobresaliente,
+                    'indicadorable_id' => $facultad,
+                    'indicadorable_type' => 'App\Models\Facultad',
+                    'indicador_id' => $this->indicador_id
+                ]);
+            }
+        }
+        if (count($this->escuelas_selected) > 0) {
+            foreach ($this->escuelas_selected as $escuela) {
+                $esc_abrev = Escuela::select('abrev')->where('id', $escuela)->first();
+                Indicadorable::create([
+                    'cod_ind_final' => ($this->indicador->cod_ind_inicial . '-' . $esc_abrev->abrev),
+                    'minimo' => $this->indicador->minimo,
+                    'sobresaliente' => $this->indicador->sobresaliente,
+                    'indicadorable_id' => $facultad,
+                    'indicadorable_type' => 'App\Models\Escuela',
+                    'indicador_id' => $this->indicador_id
+                ]);
+            }
+        }
+        $this->emit('guardado', ['titulo' => 'Entidad quitado', 'mensaje' => 'guardado']);
     }
 }
--- resources/views/livewire/admin/asignar-indicador.blade.php
+++ resources/views/livewire/admin/asignar-indicador.blade.php
@@ -89,4 +89,164 @@
         </div>
     </div>
 
+    @if(!is_null($facultades_not_indicador) or !is_null($escuelas_not_indicador))
+        <x-jet-dialog-modal wire:model="open" maxWidth="4xl">
+            <x-slot name="title">
+                <div class="flex justify-between w-full">
+                    <h1 class="font-bold text-zinc-700">
+                        Asignar el indicador a facultades y/o escuela
+                    </h1>
+                    <x-utils.buttons.close-button wire:click="$set('open', false)"/>
+                </div>
+            </x-slot>
+
+            <x-slot name="content">
+
+                <div class="grid grid-cols-2 gap-x-4 items-start">
+                    {{-- Sección de facultad --}}
+                    <section class="border border-zinc-300 divide-y divide-zinc-300 rounded-md text-zinc-700">
+                        <header class="px-3 py-2 rounded-t-md bg-stone-100">
+                            <h2 class="font-bold text-zinc-600 mr-1">Sección de Facultades</h2>
+                            <p class="text-sm">
+                                @if(count($facultades_selected))
+                                    (
+                                    {{ count($facultades_selected) }} {{ count($facultades_selected) == 1 ? 'facultad seleccionado' : 'facultades seleccionados' }}
+                                    )
+                                @else
+                                    (No haz seleccionado ninguna facultad)
+                                @endif
+                            </p>
+                        </header>
+
+                        <div class="divide-y divide-zinc-300">
+                            <table class="divide-y divide-zinc-300 w-full overflow-hidden">
+                                <tbody class="text-sm text-zinc-700 divide-y divide-zinc-300">
+                                @foreach($facultades_not_indicador as $facultad)
+                                    <tr class="hover:bg-zinc-100 soft-transition">
+                                        <td class="px-3 py-1.5 text-left ">
+                                            <x-utils.forms.checkbox wire:model="facultades_selected"
+                                                                    wire:loading.attr="disabled"
+                                                                    value="{{ $facultad->id }}"/>
+                                        </td>
+                                        <td class="px-3 py-1.5 text-left">{{ $facultad->nombre }}</td>
+                                    </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </section>
+
+                    {{-- Sección de entidades --}}
+                    <section class="border border-zinc-300 divide-y divide-zinc-300 rounded-md text-zinc-700">
+                        <header class="px-3 py-2 rounded-t-md bg-stone-100">
+                            <h2 class="font-bold text-zinc-600 mr-1">Sección de Entidades</h2>
+                            <p class="text-sm">
+                                @if(count($escuelas_selected))
+                                    (
+                                    {{ count($escuelas_selected) }} {{ count($escuelas_selected) == 1 ? 'programa de estudio seleccionado' : 'programa des estudios seleccionados' }}
+                                    )
+                                @else
+                                    (No haz seleccionado ninguna escuela)
+                                @endif
+                            </p>
+                        </header>
+
+                        <div class="divide-y divide-zinc-300">
+                            @if($escuelas_not_indicador)
+                                <table class="divide-y divide-zinc-300 w-full overflow-hidden">
+                                    <tbody class="text-sm text-zinc-700 divide-y divide-zinc-300">
+                                    @foreach($escuelas_not_indicador as $escuelas)
+                                        <tr class="hover:bg-zinc-100 soft-transition">
+                                            <td class="px-3 py-1.5 text-left">
+                                                <x-utils.forms.checkbox wire:model="escuelas_selected"
+                                                                        wire:loading.attr="disabled"
+                                                                        value="{{ $escuelas->id }}"/>
+                                            </td>
+                                            <td class="px-3 py-1.5 text-left">{{ $escuelas->nombre }}</td>
+                                        </tr>
+                                    @endforeach
+                                    </tbody>
+                                </table>
+                            @else
+                                <div class="w-full grid place-items-center py-8">
+                                    <div class="w-1/2 flex flex-col justify-between items-center">
+                                        <svg class="text-zinc-500 fill-current mx-auto" viewBox="0 0 24 24" width="24"
+                                             height="24">
+                                            <path
+                                                d="M3.5 3.75a.25.25 0 01.25-.25h13.5a.25.25 0 01.25.25v10a.75.75 0 001.5 0v-10A1.75 1.75 0 0017.25 2H3.75A1.75 1.75 0 002 3.75v16.5c0 .966.784 1.75 1.75 1.75h7a.75.75 0 000-1.5h-7a.25.25 0 01-.25-.25V3.75z"></path>
+                                            <path
+                                                d="M6.25 7a.75.75 0 000 1.5h8.5a.75.75 0 000-1.5h-8.5zm-.75 4.75a.75.75 0 01.75-.75h4.5a.75.75 0 010 1.5h-4.5a.75.75 0 01-.75-.75zm16.28 4.53a.75.75 0 10-1.06-1.06l-4.97 4.97-1.97-1.97a.75.75 0 10-1.06 1.06l2.5 2.5a.75.75 0 001.06 0l5.5-5.5z"></path>
+                                        </svg>
+                                        <p class="text-zinc-800 leading-5 text-center mt-2">
+                                            Seleccione una facultad para visualizar escuelas
+                                        </p>
+                                    </div>
+                                </div>
+                            @endif
+                        </div>
+                    </section>
+                </div>
+            </x-slot>
+
+            <x-slot name="footer">
+                <x-jet-button
+                    wire:click="asignarIndicador"
+                    wire:target="asignarIndicador"
+                    wire:loading.class="cursor-wait"
+                    wire:loading.attr="disabled">
+                    <x-icons.load wire:loading wire:target="asignarIndicador" class="icon-5"/>
+                    Guardar
+                </x-jet-button>
+            </x-slot>
+        </x-jet-dialog-modal>
+    @endif
+
+
+    @push('js')
+        <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+        <script>
+            function eliminarRol(rol_nombre, rol_id) {
+                Swal.fire({
+                    html: `<b>¿Desea quitar el rol ${rol_nombre} del usuario?</b><br/><small>Nota: Se eliminarán las entidades correspondientes</small>`,
+                    icon: 'warning',
+                    showCancelButton: true,
+                    confirmButtonText: 'Si, eliminar',
+                    cancelButtonText: `Cancelar`,
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        window.livewire.emit('eliminarRol', rol_nombre, rol_id);
+                    }
+                })
+            }
+
+            function eliminarEntidad(entidad_id, entidad_nombre, rol_nombre) {
+                Swal.fire({
+                    html: `<b>¿Desea quitar la entidad ${entidad_nombre} del usuario?</b><br/><small>Nota: Se eliminará el rol correspondientes</small>`,
+                    icon: 'warning',
+                    showCancelButton: true,
+                    confirmButtonText: 'Si, eliminar',
+                    cancelButtonText: `Cancelar`,
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        window.livewire.emit('eliminarEntidad', entidad_id, entidad_nombre, rol_nombre);
+                    }
+                })
+            }
+
+            Livewire.on('guardado', rspta => {
+                Swal.fire({
+                    html: `<b>!${rspta.titulo}!</b><br/><small>${rspta.mensaje}</small>`,
+                    icon: 'success'
+                });
+            });
+
+            Livewire.on('error', msg => {
+                Swal.fire({
+                    html: `<b>!Hubo un error!</b><br/><small>${msg}</small>`,
+                    icon: 'error'
+                });
+            });
+        </script>
+    @endpush
+
 </div>
