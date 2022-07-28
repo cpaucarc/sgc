71fceb5f144a1b90020f0eda0d92be6c491a7a1c
Frank Paucar <fcpaucar@gmail.com>
2022-07-27 20:51:33 -0500

    :card_file_box: agrega los seeders de responsables-salidas y clientes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -27,7 +27,7 @@ class DatabaseSeeder extends Seeder
         $this->call(FinanciadorSeeder::class);
         $this->call(FrecuenciaSeeder::class);
         $this->call(GradoAcademicoSeeder::class);
-        \App\Models\Investigador::factory(50)->create();
+        //    \App\Models\Investigador::factory(50)->create();
         $this->call(ProcesoSeeder::class);
         $this->call(SemestreSeeder::class);
         $this->call(TipoActividadSeeder::class);
@@ -37,7 +37,7 @@ class DatabaseSeeder extends Seeder
         $this->call(UnidadMedidaSeeder::class);
         $this->call(RoleSeeder::class);
         $this->call(UserSeeder::class);
-        \App\Models\Empresa::factory(50)->create();
+        //      \App\Models\Empresa::factory(50)->create();
 
         // Nivel 1
         $this->call(ActividadSeeder::class);
@@ -47,45 +47,46 @@ class DatabaseSeeder extends Seeder
         $this->call(EscuelaSeeder::class);
         $this->call(EstadoSeeder::class);
         //Factory de GradoEstudiante
-        \App\Models\Institucion::factory(25)->create();
-        \App\Models\Jurado::factory(25)->create();
+//        \App\Models\Institucion::factory(25)->create();
+//        \App\Models\Jurado::factory(25)->create();
         $this->call(RequisitoSeeder::class);
         $this->call(SalidaSeeder::class);
 
         //Solicitudes
-        \App\Models\GradoEstudiante::factory(350)->create();
-        \App\Models\Solicitud::factory(300)->create();
+//        \App\Models\GradoEstudiante::factory(350)->create();
+//        \App\Models\Solicitud::factory(300)->create();
 
         // Nivel 2
-//        $this->call(ConvalidacionSeeder::class);
+//      //  $this->call(ConvalidacionSeeder::class);
         $this->call(EntidadSeeder::class);
-        \App\Models\EstudianteExterno::factory(50)->create();
+//        \App\Models\EstudianteExterno::factory(50)->create();
         $this->call(IndicadorSeeder::class);
         $this->call(LineaInvestigacionSeeder::class);
-        \App\Models\ResponsabilidadSocial::factory(50)->create();
-        \App\Models\Tesis::factory(50)->create();
+//        \App\Models\ResponsabilidadSocial::factory(50)->create();
+//        \App\Models\Tesis::factory(50)->create();
         $this->call(CicloSeeder::class);
         $this->call(CursoSeeder::class);
 
 
         // Nivel 3
-//        \App\Models\ConvalidacionPostulante::factory(75)->create();
+//      //  \App\Models\ConvalidacionPostulante::factory(75)->create();
         $this->call(EntidadableSeeder::class);
         $this->call(IndicadorableSeeder::class);
         $this->call(ResponsableSeeder::class);
-        \App\Models\RsuParticipante::factory(75)->create();
+//        \App\Models\RsuParticipante::factory(75)->create();
         $this->call(SublineaInvestigacionSeeder::class);
-        \App\Models\Sustentacion::factory(30)->create();
+//        \App\Models\Sustentacion::factory(30)->create();
 
         // Nivel 4
+        $this->call(ResponsableSalidaSeeder::class);
         $this->call(ClienteSeeder::class);
-        \App\Models\Investigacion::factory(30)->create();
-        \App\Models\JuradoSustentacion::factory(90)->create();
+//        \App\Models\Investigacion::factory(30)->create();
+//        \App\Models\JuradoSustentacion::factory(90)->create();
         $this->call(ProveedorSeeder::class);
 
         // Nivel 5
-        \App\Models\InvestigacionFinanciacion::factory(90)->create();
-        \App\Models\InvestigacionInvestigador::factory(90)->create();
+//        \App\Models\InvestigacionFinanciacion::factory(90)->create();
+//        \App\Models\InvestigacionInvestigador::factory(90)->create();
 
         //Sprint 3
         $this->call(ServicioSeeder::class);
