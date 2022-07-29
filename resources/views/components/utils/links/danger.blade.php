2ff76007dbf9987f76e7ba7a5d74543f6037d75c
Frank Paucar <fcpaucar@gmail.com>
2022-07-29 17:18:50 -0500

    :lipstick: agrega la clase text-sm al link danger

--- resources/views/components/utils/links/danger.blade.php
+++ resources/views/components/utils/links/danger.blade.php
@@ -1,3 +1,3 @@
-<a {{ $attributes->merge([ 'class' => 'btn text-rose-600 hover:text-white hover:bg-rose-700 active:bg-rose-600 border hover:border-rose-600' ]) }}>
+<a {{ $attributes->merge([ 'class' => 'btn text-sm text-rose-600 hover:text-white hover:bg-rose-700 active:bg-rose-600 border hover:border-rose-600' ]) }}>
     {{$slot}}
 </a>
