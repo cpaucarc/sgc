a0174a1efa93d23201aeec0c3c3674cf62845d4a
Angel Pagola <57104299+angelpagola@users.noreply.github.com>
2022-07-29 10:46:42 -0500

    :seedling: Actualiza los requisitos de tpu y bachiller

--- database/seeders/RequisitoSeeder.php
+++ database/seeders/RequisitoSeeder.php
@@ -17,96 +17,116 @@ class RequisitoSeeder extends Seeder
         $requisitos = [
             // Proceso de Grado Bachiller (15)
             [
-                'nombre' => 'FUT dirigida al decano solicitando grado de bachiller',
+                'nombre' => 'FUT DIRIGIDO AL DECANO SOLICITANDO GRADO DE BACHILLER',
                 'proceso_id' => 12 // 12: Grado Bachiller (Tabla: Procesos)
             ],
             [
-                'nombre' => 'Certificado de estudios originales, (OGE)',
+                'nombre' => 'CERTIFICADOS DE ESTUDIOS ORIGINALES, (expedido por la OGE. (del I al X ciclo)',
                 'proceso_id' => 12 // 12: Grado Bachiller (Tabla: Procesos)
             ],
             [
-                'nombre' => 'Constancia de egresado (dirección de escuela respectiva)',
+                'nombre' => 'CONSTANCIA DE EGRESADO, (expedida por la dirección de escuela respectiva, (autenticada por Secretaria General de la UNASAM)',
                 'proceso_id' => 12 // 12: Grado Bachiller (Tabla: Procesos)
             ],
             [
-                'nombre' => 'Constancia original de ingreso (Comisión Central de Admisión de la UNASAM)',
+                'nombre' => 'CONSTANCIA ORIGINAL DE INGRESO (expedida por la Comisión Central de Admisión (CCA) de la UNASAM',
                 'proceso_id' => 12 // 12: Grado Bachiller (Tabla: Procesos)
             ],
             [
-                'nombre' => 'Recibo de pago por derecho de grado académico de bachiller, determinado en el Tupa',
+                'nombre' => 'RECIBO DE PAGO POR DERECHO DE GRADO ACADÉMICO DE BACHILLER, determinado en el TUPA',
                 'proceso_id' => 12 // 12: Grado Bachiller (Tabla: Procesos)
             ],
             [
-                'nombre' => 'Copia de DNI simple (Legalizado)',
+                'nombre' => 'COPIA SIMPLE DE DNI (legalizado)',
                 'proceso_id' => 12 // 12: Grado Bachiller (Tabla: Procesos)
             ],
             [
-                'nombre' => 'Copia simple de partida de nacimiento (Legalizado)',
+                'nombre' => 'COPIA SIMPLE DE LA PARTIDA DE NACIMIENTO (legalizado)',
                 'proceso_id' => 12 // 12: Grado Bachiller (Tabla: Procesos)
             ],
             [
-                'nombre' => 'Copia de certificado de estudios de conocimientod de idiomas nivel básico (UNASAM)',
+                'nombre' => 'COPIA DEL CERTIFICADO DE ESTUDIOS DE CONOCIMIENTO DEL IDIOMA INGLÉS O QUECHUA A NIVEL BÁSICO, otorgado por el centro de idiomas de la UNASAM, (autentificado por secretaria general de la UNASAM)',
                 'proceso_id' => 12 // 12: Grado Bachiller (Tabla: Procesos)
             ],
             [
-                'nombre' => 'Constancia de práctica (s) pre profesional (es), (dirección de escuela)',
+                'nombre' => 'CONSTANCIA DE PRACTICA (S) PRE PROFESIONAL (ES) (expedida por la Dirección de Escuela)',
                 'proceso_id' => 12 // 12: Grado Bachiller (Tabla: Procesos)
             ],
             [
-                'nombre' => 'Constancia de no adeudar bienes a la biblioteca central de la UNASAM',
+                'nombre' => 'CONSTANCIA DE NO ADEUDAR BIENES A LA BIBLIOTECA CENTRAL DE LA UNASAM',
                 'proceso_id' => 12 // 12: Grado Bachiller (Tabla: Procesos)
             ],
             [
-                'nombre' => 'Constancia de no adeudar bienes a la dirección de bienestar universitario de la UNASAM',
+                'nombre' => ' CONSTANCIA DE NO ADEUDO A LA DIRECCIÓN DE BIENESTAR UNIVERSITARIO DE LA UNASAM',
                 'proceso_id' => 12 // 12: Grado Bachiller (Tabla: Procesos)
             ],
             [
-                'nombre' => 'Constancia de no adeudar bienes y/o valores a la facultad',
+                'nombre' => ' CONSTANCIA DE NO ADEUDAR BIENES Y/O VALORES A LA FACULTAD',
                 'proceso_id' => 12 // 12: Grado Bachiller (Tabla: Procesos)
             ],
             [
-                'nombre' => 'Constancia original de primera matrícula',
+                'nombre' => 'CONSTANCIA ORIGINAL DE PRIMERA MATRICULA, con la rúbrica del Director de Escuela',
                 'proceso_id' => 12 // 12: Grado Bachiller (Tabla: Procesos)
             ],
             [
-                'nombre' => 'Constancia original de fecha exacta de egreso',
+                'nombre' => 'CONSTANCIA ORIGINAL DE FECHA EXACTA DE EGRESO. Con rúbrica del Director de Escuela',
                 'proceso_id' => 12 // 12: Grado Bachiller (Tabla: Procesos)
             ],
             [
-                'nombre' => 'Dos fotografias de frente, tamaño pasaporte.',
+                'nombre' => 'DOS FOTOGRAFÍAS DE FRENTE, TAMAÑO PASAPORTE A COLORES CON FONDO BLANCO, RECIENTES',
                 'proceso_id' => 12 // 12: Grado Bachiller (Tabla: Procesos)
             ],
-            //  Proceso de Titulo Profesional (8)
+            //  Proceso de Titulo Profesional (13)
             [
-                'nombre' => 'Copia de diploma de bachiller',
+                'nombre' => 'SOLICITUD DIRIGIDA AL DECANO DE LA FACULTAD DE CIENCIAS (FUT)',
                 'proceso_id' => 5 // 5: Titulo Profesional (Tabla: Procesos)
             ],
             [
-                'nombre' => 'Solicitud de inscripción por plan de tesis',
+                'nombre' => 'RECIBO DE PAGO POR DERECHO DE TÍTULO PROFESIONAL, determinado por el TUPA',
                 'proceso_id' => 5 // 5: Titulo Profesional (Tabla: Procesos)
             ],
             [
-                'nombre' => 'Plan de tesis',
+                'nombre' => 'COPIA DEL GRADO ACADÉMICO DE BACHILLER, autenticada por Secretaría General de la UNASAM.',
                 'proceso_id' => 5 // 5: Titulo Profesional (Tabla: Procesos)
             ],
             [
-                'nombre' => 'Constancia de aprobación plan de tesis',
+                'nombre' => 'COPIA DEL ACTA DE SUSTENTACIÓN Y APROBACIÓN DE LA TESIS O PROGRAMA DE TITULACIÓN DE TESIS GUIADA O TRABAJO DE SUFICIENCIA PROFESIONAL (autenticada por Secretaría General de la UNASAM).',
                 'proceso_id' => 5 // 5: Titulo Profesional (Tabla: Procesos)
             ],
             [
-                'nombre' => 'Solicitud de designación de jurados',
+                'nombre' => 'COPIA DE CONSTANCIA DE EGRESO, autenticada por la Secretaría General de la UNASAM',
                 'proceso_id' => 5 // 5: Titulo Profesional (Tabla: Procesos)
             ],
             [
-                'nombre' => 'Proyecto de investigación',
+                'nombre' => 'COPIA DE CERTIFICADO DE ESTUDIOS, autenticado por la Secretaría General de la UNASAM',
                 'proceso_id' => 5 // 5: Titulo Profesional (Tabla: Procesos)
             ],
             [
-                'nombre' => 'Constancia de no adeudar',
+                'nombre' => 'COPIA DE DNI (legalizado)',
                 'proceso_id' => 5 // 5: Titulo Profesional (Tabla: Procesos)
             ],
             [
-                'nombre' => 'Certificado de prácticas pre-profesionales',
+                'nombre' => 'COPIA DE CONSTANCIA DE PRIMERA MATRÍCULA',
+                'proceso_id' => 5 // 5: Titulo Profesional (Tabla: Procesos)
+            ],
+            [
+                'nombre' => 'COPIA DE CONSTANCIA DE FECHA EXACTA DE EGRESO',
+                'proceso_id' => 5 // 5: Titulo Profesional (Tabla: Procesos)
+            ],
+            [
+                'nombre' => 'INCORPORAR LA HOJA DE RENATI, CON RÚBRICA EN DIGITAL E IMPRESO COMO PARTE DEL ANEXO EN LA TESIS. (incorporar impreso para el expediente)',
+                'proceso_id' => 5 // 5: Titulo Profesional (Tabla: Procesos)
+            ],
+            [
+                'nombre' => 'CONSTANCIA DE NO ADEUDAR BIENES Y/O VALORES A LA FACULTAD',
+                'proceso_id' => 5 // 5: Titulo Profesional (Tabla: Procesos)
+            ],
+            [
+                'nombre' => 'CONSTANCIA DE NO ADEUDO A LA BIBLIOTECA CENTRAL DE LA UNASAM',
+                'proceso_id' => 5 // 5: Titulo Profesional (Tabla: Procesos)
+            ],
+            [
+                'nombre' => 'DOS (02) FOTOGRAFÍAS DE FRENTE, TAMAÑO PASAPORTE A COLORES CON FONDO BLANCO, RECIENTES',
                 'proceso_id' => 5 // 5: Titulo Profesional (Tabla: Procesos)
             ],
             //  Proceso de Convalidaciones (4)
